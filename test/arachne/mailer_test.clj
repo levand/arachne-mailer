4990c01b6b78b2c6e81fc928bdc30d527562ba12
rubygeek <nola@rubygeek.com>
2017-01-11 15:15:21 -0600

    updated tests to be better

--- test/arachne/mailer_test.clj
+++ test/arachne/mailer_test.clj
@@ -4,34 +4,34 @@
 
 
 (deftest test-apache-commons-base-config
-  (let [config (mailer/->ApacheCommons "host" 122 "test-auth" 42)]
-    (is (= (class config) arachne.mailer.ApacheCommons))
-    (is (= (:hostname config) "host"))
-    (is (= (:smtp-port config) 122))
-    (is (= (:authenticator config) "test-auth")
-    (is (= (:tls config) 42)))))
+  (let [mailer (mailer/->ApacheCommons "host" 122 "test-auth" 42)]
+    (is (= (class mailer) arachne.mailer.ApacheCommons))
+    (is (= (:hostname mailer) "host"))
+    (is (= (:smtp-port mailer) 122))
+    (is (= (:authenticator mailer) "test-auth")
+    (is (= (:tls mailer) 42)))))
 
 (deftest test-apache-commons-combined-config
-  (let [config (mailer/->ApacheCommons "host" 122 "test-auth" 42)
-        big (mailer/sendmail! config "admin@rubygeek.com" "tacos" "yummy tacos")] 
-    (is (= (:tls big) 42))
-    (is (= (:hostname big) "host"))
-    (is (= (:subject big) "tacos"))
-    (is (= (:body big) "yummy tacos"))))
+  (let [mailer (-> (mailer/->ApacheCommons "host" 122 "test-auth" 42)
+                   (mailer/sendmail! "admin@rubygeek.com" "tacos" "yummy tacos"))] 
+    (is (= (:tls mailer) 42))
+    (is (= (:hostname mailer) "host"))
+    (is (= (:subject mailer) "tacos"))
+    (is (= (:body mailer) "yummy tacos"))))
               
 
 (deftest test-amazon-ses-mail-config
-  (let [config (mailer/->SesMailer "aws" "access-key-number" "secret-key-number" :aws-east1)]
-    (is (= (class config) arachne.mailer.SesMailer))
-    (is (= (:provider config) "aws"))
-    (is (= (:access-key config) "access-key-number"))
-    (is (= (:secret-key config) "secret-key-number"))
-    (is (= (:region config) :aws-east1))))
+  (let [mailer (mailer/->SesMailer "aws" "access-key-number" "secret-key-number" :aws-east1)]
+    (is (= (class mailer) arachne.mailer.SesMailer))
+    (is (= (:provider mailer) "aws"))
+    (is (= (:access-key mailer) "access-key-number"))
+    (is (= (:secret-key mailer) "secret-key-number"))
+    (is (= (:region mailer) :aws-east1))))
 
 (deftest test-amazon-ses-mail-combined-config
-  (let [config (mailer/->SesMailer "aws" "access-key-number" "secret-key-number" :aws-east1)
-        big (mailer/sendmail! config "admin@rubygeek.com" "tacos" "yummy tacos")]
-    (is (= (:access-key big) "access-key-number"))
-    (is (= (:region big) :aws-east1))
-    (is (= (:subject big) "tacos"))
-    (is (= (:body big) "yummy tacos"))))
+  (let [mailer (-> (mailer/->SesMailer "aws" "access-key-number" "secret-key-number" :aws-east1)
+                   (mailer/sendmail! "admin@rubygeek.com" "tacos" "yummy tacos"))]
+    (is (= (:access-key mailer) "access-key-number"))
+    (is (= (:region mailer) :aws-east1))
+    (is (= (:subject mailer) "tacos"))
+    (is (= (:body mailer) "yummy tacos"))))
